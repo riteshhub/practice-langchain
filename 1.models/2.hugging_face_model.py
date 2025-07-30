addf2af473b65644db9b19ca431b069762eb72e0
riteshhub <patel.ritesh0102@gmail.com>
2025-07-30 10:58:05 +0530

    corrected path

--- 1.models/2.hugging_face_model.py
+++ 1.models/2.hugging_face_model.py
@@ -2,17 +2,17 @@ import os
 from dotenv import load_dotenv
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
-os.environ["HF_HOME"] = "C:\Users\Ritesh_Patel\Documents\learning\AI\hugging_face_models"
+os.environ["HF_HOME"] = "C:/Users/Ritesh_Patel/Documents/learning/AI/hugging_face_models"
 
 load_dotenv()
 
-llm = HuggingFaceEndpoint(
+model = HuggingFaceEndpoint(
     repo_id="HuggingFaceTB/SmolLM3-3B",
     task="text-generation"
 )
 
-model = ChatHuggingFace(llm=llm, verbose=True)
+llm = ChatHuggingFace(llm=model, verbose=True)
 
-result = model.invoke(input="What are the most spoken languages in the world?")
+result = llm.invoke(input="What are the most spoken languages in the world?")
 
 print(result)
