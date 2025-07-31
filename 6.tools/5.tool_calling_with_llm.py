c1bb18afd9c98f79895d72ae9ab40631174a0185
riteshhub <patel.ritesh0102@gmail.com>
2025-07-31 13:13:15 +0530

    changes in tool calling

--- 6.tools/5.tool_calling_with_llm.py
+++ 6.tools/5.tool_calling_with_llm.py
@@ -9,18 +9,25 @@ def multiply(a: int, b: int) -> int:
     "multiply given numbers"
     return a*b
 
+@tool
+def division(a: int, b: int) -> int:
+    "divide given numbers"
+    return a/b
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0.1,
     max_output_tokens=1024
 )
 
-llm_with_tools = llm.bind_tools([multiply])
+llm_with_tools = llm.bind_tools([multiply,division])
 
 # result = llm_with_tools.invoke("How are you?")
-result = llm_with_tools.invoke("perform multiplication of 5 with 6")
+result = llm_with_tools.invoke("What is the division of 6 and 2?")
 
-if hasattr(result, "tool_calls"):
-    result = multiply.invoke(result.tool_calls[0])
+print(result)
 
-print(result.content)
+# if result.content=='':
+#     result = multiply.invoke(result.tool_calls[0])
+
+# print(result.content)
